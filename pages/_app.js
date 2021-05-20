9a509b95fe312eea537de5cb15aee9e0053af02d
blorence <blorence@agency1903.com>
2021-05-19 22:37:48 -0400

    Functional Locally

--- pages/_app.js
+++ pages/_app.js
@@ -1,69 +1,30 @@
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import React from "react";
 import App from "next/app";
+import Head from "next/head";
 import { AppProvider } from "@shopify/polaris";
-import { Provider, useAppBridge } from "@shopify/app-bridge-react";
-import { authenticatedFetch } from "@shopify/app-bridge-utils";
-import { Redirect } from "@shopify/app-bridge/actions";
+import { Provider } from "@shopify/app-bridge-react";
 import "@shopify/polaris/dist/styles.css";
 import translations from "@shopify/polaris/locales/en.json";
-
-function userLoggedInFetch(app) {
-  const fetchFunction = authenticatedFetch(app);
-
-  return async (uri, options) => {
-    const response = await fetchFunction(uri, options);
-
-    if (
-      response.headers.get("X-Shopify-API-Request-Failure-Reauthorize") === "1"
-    ) {
-      const authUrlHeader = response.headers.get(
-        "X-Shopify-API-Request-Failure-Reauthorize-Url"
-      );
-
-      const redirect = Redirect.create(app);
-      redirect.dispatch(Redirect.Action.APP, authUrlHeader || `/auth`);
-      return null;
-    }
-
-    return response;
-  };
-}
-
-function MyProvider(props) {
-  const app = useAppBridge();
-
-  const client = new ApolloClient({
-    fetch: userLoggedInFetch(app),
-    fetchOptions: {
-      credentials: "include",
-    },
-  });
-
-  const Component = props.Component;
-
-  return (
-    <ApolloProvider client={client}>
-      <Component {...props} />
-    </ApolloProvider>
-  );
-}
+import ClientRouter from "../components/ClientRouter";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, shopOrigin } = this.props;
+
+    const config = { apiKey: API_KEY, shopOrigin, forceRedirect: true };
     return (
-      <AppProvider i18n={translations}>
-        <Provider
-          config={{
-            apiKey: API_KEY,
-            shopOrigin: shopOrigin,
-            forceRedirect: true,
-          }}
-        >
-          <MyProvider Component={Component} {...pageProps} />
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <Provider config={config}>
+          <ClientRouter />
+          <AppProvider i18n={translations}>
+            <Component {...pageProps} />
+          </AppProvider>
         </Provider>
-      </AppProvider>
+      </React.Fragment>
     );
   }
 }
